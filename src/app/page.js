1b3af837538136a23a1036f9152712222b3836c6
emilio <emilio.lihue.q@gmail.com>
2023-06-16 22:15:35 -0300

    suite landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,93 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import utils from '@/styles/_utils.module.scss'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+    <main className={utils.heightI}>
+      <header className={`${utils.header} ${utils.dflex} ${utils.marginTB_auto} ${utils.maxWidthM} `}>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <h1>suite</h1>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div>
+          <span className={utils.btnSecondary}>Request Beta Access</span>
+        </div>
+      </header>
+      <section className={`${utils.hero} ${utils.marginL}`}>
+        <div className={`${utils.heroContent} ${utils.marginL}`}>
+          <div className={`${utils.line}`}></div>
           <h2>
-            Docs <span>-&gt;</span>
+            A <b>super solution
+            for your business.</b>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className={`${styles.heroText} ${utils.marginTB} ${utils.lineHeightM}`}>
+            Our marketing and sales automations help you scale your outreach to get more leads for your company. 
           </p>
-        </a>
-      </div>
+          <span className={utils.btnPrimary}>Request Beta Access</span>
+        </div>
+        <div className={`${utils.stats} ${styles.grid} ${utils.marginTB}`}>
+          <div className={styles.imageContent}>
+            <picture>
+              <source srcSet="/images/image-hero-landscape.png" media="(max-width: 768px)" />
+              <source srcSet="/images/image-hero-portrait.png" width={281} height={482} />
+              <Image src="/images/image-hero-landscape.png" priority={true} alt="My Image" width={343} height={240} />
+            </picture>
+          </div>
+          <div className={`${styles.card} ${styles.companies} ${utils.marginTM}`}>
+            <h2>
+              <span>2k +</span>
+            </h2>
+            <p>COMPANIES.</p>
+          </div>
+          <div className={`${styles.card} ${styles.langs} ${utils.marginTM}`}>
+            <h2>
+            <span>8 </span>
+            </h2>
+            <p>Languages</p>
+          </div>
+          <div className={`${styles.card} ${styles.leads}  ${utils.marginTM}`}>
+            <h2>
+            <span>1.2 M</span>
+            </h2>
+            <p>LEADS</p>
+          </div>
+        </div>
+      </section>
+      <section className={`${utils.paddingM} ${utils.testimonials} ${utils.relative}`}>
+        <div className={`${utils.testimonialsWraper} ${utils.maxWidthM} ${utils.marginAuto}`}>
+          <picture>
+            <source srcSet="/images/image-jeremy-small.png"  media="(max-width: 768px)" />
+            <source srcSet="/images/image-jeremy-large.png" width={361} height={590} /> 
+            <Image className={`${utils.marginAuto} ${utils.testimonialsImage}`} src="/images/image-jeremy-small.png" priority={true} alt="My Image" width={243} height={395} />
+          </picture>
+          <div className={`${utils.line} ${utils.curved2}`}></div>
+          <h3>It just <b>works</b>.</h3>
+          <p className={`${utils.marginTB} ${utils.lineHeightM}`}>“I really like how it is an all-in-one solution that handle many of the tasks that you would normally need separate tools to do the same job. This thing is a miracle worker.”</p>
+          <h5>JEREMY ROBINSON</h5>
+          <p className={utils.marginTS}>CMO, FYLO</p>
+        </div>
+      </section>
+      <footer className={`${utils.footer}`}>
+        <div>
+          <h5>suite</h5>
+        </div>
+        <div className={`${utils.marginTB_small}`}>
+          <span>Copyright - Suite</span>
+        </div>
+        <div>
+          <ul className={`${utils.dflex} ${utils.justifyCenter} ${utils.listStyleNone}`}>
+            <li className={utils.marginRyL}>
+              <Image src="/images/icon-facebook.svg"  alt="facebook" width={20} height={20} />
+            </li>
+            <li className={utils.marginRyL}>
+              <Image src="/images/icon-instagram.svg"  alt="instagram" width={20} height={20} />
+            </li>
+            <li className={utils.marginRyL}>
+            <Image src="/images/icon-twitter.svg"  alt="twitter" width={20} height={20} />
+            </li>
+          </ul>
+        </div>
+      </footer>
     </main>
   )
 }
